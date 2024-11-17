cbdbe67104408ed4f4c5e92aef9dc091251a91f8
DELL PRO <goluwafemi2009@gmail.com>
2024-11-17 16:11:05 +0100

    Add web form

--- send_email.py
+++ send_email.py
@@ -1,19 +1,16 @@
 import smtplib, ssl
-from wsgiref.simple_server import server_version
 
-username = "femigbenga3470@gmail.com"
-password = "fqzd ittr hgwp dfvb"
-receiver = "femigbenga3470@gmail.com"
 
-host = "smtp.gmail.com"
-post = 465
-message = '''\
-Subject: Hi! 
-You are accepted
-Bye!
-'''
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(host, post, context = context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+def send_email(message):
+    username = "femigbenga3470@gmail.com"
+    password = "fqzd ittr hgwp dfvb"
+    receiver = "femigbenga3470@gmail.com"
+
+    host = "smtp.gmail.com"
+    post = 465
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, post, context = context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
